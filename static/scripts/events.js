78826119f47d84b24420328040fe902c9e2e6980
Scott Wysocki <swysocki@gmail.com>
2021-08-18 07:40:39 -0400

    wip: start string

--- static/scripts/events.js
+++ static/scripts/events.js
@@ -1,17 +1,43 @@
-var eventData
+var eventData;
+
+function stringMonth(eventDate) {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return monthNames[eventDate.getMonth()];
+}
+
+function getDates(startDate, endDate) {
+  let start = new Date(startDate);
+  let end = new Date(endDate);
+  startString = `${stringMonth(start)} ${start.getDate()}, ${start.getFullYear()}`;
+  return startString
+}
 
 function eventDisplay(pbEvent) {
   for (item of pbEvent) {
     container = document.getElementById("event-container");
     var title = document.createElement("h3");
-    title.innerHTML = item.name;
+    title.innerHTML = `${item.name}<small><span class="icon-location"></span>${item.location}</small>`;
     container.appendChild(title);
     var para = document.createElement("p");
-    para.innerHTML = 
-      `${item.start_date} - ${item.end_date}<br />
-      <b>${item.location}</b><br />
+    dateString = getDates(item.start_date, item.end_date);
+    para.innerHTML = `${dateString}<br />
+      <span class="icon-location"></span><b>${item.location}</b><br />
       ${item.address}<br />
-      ${item.city}<br />`;
+      ${item.city}<br />
+      <a href=${item.url}>Event Link</a>`;
     container.appendChild(para);
   }
 }
@@ -20,10 +46,10 @@ function getEvents() {
   let events = new Set();
   for (pbEvent of eventData) {
     if (Date.parse(pbEvent.start_date) >= Date.now()) {
-      events.add(pbEvent)
+      events.add(pbEvent);
     }
   }
-  console.log(events)
+  console.log(events);
   return events;
 }
 
